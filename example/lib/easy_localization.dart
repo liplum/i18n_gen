3f953869ef5ead685c526dbec25a85f584ae0a8f
liplum <Li_plum@outlook.com>
2025-02-14 11:20:54 +0800

    feat: Generate typed Dart code for nested translation keys
    This commit introduces the ability to generate typed Dart code for nested translation keys in `easy_localization`.
    
    - Modified `lib/easy_localization/generate.dart` to:
        - Generate Dart classes for nested translation keys, providing type-safe access.
        - Use the new Node hierarchy to handle nested keys.
        - Added `ObjectNode` and `ValueNode` classes to represent nodes in the localization data structure.
        - Added `Node` class and `listAllNodes`, `listAllObjectNodes` functions to build and traverse the node tree.
        - Generate Dart code based on the parsed node tree.
        - Added `toPascalCase` method for key names.
    - Added `example/lib/easy_localization.dart` to show an example of the generated code with nested keys.
    - Updated `example/lib/easy_localization.dart` to:
        -  Change the example of the generated code with nested keys.
        - added new code for type safe translation.

--- example/lib/easy_localization.dart
+++ example/lib/easy_localization.dart
@@ -22,54 +22,44 @@ home:
 
 class I18n {
   String get topLevel => "topLevel".tr();
-  final app = const I18n$app._("app");
-  final index = const I18n$index._("app");
+  final app = const I18n$app();
+  final index = const I18n$index();
 }
 
 class I18n$app {
-  final String ns;
+  const I18n$app();
 
-  const I18n$app._(this.ns);
+  String get name => "app.i18n_gen".tr();
 
-  String get name => "$ns.i18n_gen".tr();
-
-  String get author => "$ns.author".tr();
+  String get author => "app.author".tr();
 }
 
 class I18n$index {
-  final String ns;
+  const I18n$index();
 
-  const I18n$index._(this.ns);
+  String get title => "index.title".tr();
 
-  String get title => "$ns.title".tr();
-
-  String get desc => "$ns.desc".tr();
+  String get desc => "index.desc".tr();
 }
 
 class I18n$home {
-  final String ns;
+  const I18n$home();
 
-  const I18n$home._(this.ns);
+  String get title => "home.title".tr();
 
-  String get title => "$ns.title".tr();
-
-  String get desc => "$ns.desc".tr();
+  String get desc => "home.desc".tr();
 }
 
 class I18n$home$settings {
-  final String ns;
+  const I18n$home$settings();
 
-  const I18n$home$settings._(this.ns);
-
-  String get title => "$ns.title".tr();
+  String get title => "home.settings.title".tr();
 }
 
 class I18n$home$settings$showButton {
-  final String ns;
+  const I18n$home$settings$showButton();
 
-  const I18n$home$settings$showButton._(this.ns);
+  String get title => "home.settings.showButton.title".tr();
 
-  String get title => "$ns.title".tr();
-
-  String get desc => "$ns.desc".tr();
+  String get desc => "home.settings.showButton.desc".tr();
 }
--- lib/easy_localization/generate.dart
+++ lib/easy_localization/generate.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:convert';
 import 'dart:io';
 
 import 'package:collection/collection.dart';
@@ -66,12 +65,130 @@ String _generateDartFile(Map<String, dynamic> l10nData, GenerateOptions options)
   result.write('''
 // ignore_for_file: type=lint
 import "package:easy_localization/easy_localization.dart";
-  
-class I18n {
-  
-}
 ''');
 
+  final rootNode = Node.build(l10nData);
+  final allObjectNodes = rootNode.listAllObjectNodes();
+
+  for (final node in allObjectNodes) {
+    final className = node.buildClassName();
+    result.write(
+      "class $className {\n",
+    );
+    result.write("const $className();");
+
+    for (final subNode in node.children) {
+      if (subNode is ValueNode) {
+        result.write('String get ${subNode.key} => "${subNode.keyPath}".tr();');
+      } else if (subNode is ObjectNode) {
+        result.write("final ${subNode.key} = const ${subNode.buildClassName()}();");
+      }
+    }
+
+    result.write("}");
+  }
 
   return result.toString();
+}
+
+extension on String {
+  String toPascalCase() => split('_').map((e) {
+        if (e.isEmpty) return '';
+        if (e.length == 1) return e.toUpperCase();
+        return '${e[0].toUpperCase()}${e.substring(1).toLowerCase()}';
+      }).join('');
+}
+
+sealed class Node {
+  final ObjectNode? parent;
+  final List<String> keys;
+
+  Node({
+    this.parent,
+    required this.keys,
+  }) : assert(parent is! ValueNode, 'parent cannot be a ValueNode');
+
+  String get key => keys.last;
+
+  String get keyPath => keys.join(".");
+
+  bool get isRoot => parent == null;
+
+  static Node build(Map<String, dynamic> obj) {
+    final root = ObjectNode(keys: const []);
+    void process(Map<String, dynamic> currentObj, Node currentNode, List<String> currentKeys) {
+      for (final entry in currentObj.entries) {
+        final newKeys = [...currentKeys, entry.key];
+        if (entry.value is Map<String, dynamic>) {
+          final childNode = ObjectNode(parent: currentNode as ObjectNode, keys: newKeys);
+          currentNode.asObjectNode.children.add(childNode);
+          process(entry.value, childNode as Node, newKeys);
+        } else if (entry.value is String) {
+          final childNode = ValueNode(parent: currentNode as ObjectNode, keys: newKeys, value: entry.value);
+          currentNode.asObjectNode.children.add(childNode);
+          currentNode.children.add(ObjectNode(parent: currentNode, keys: newKeys));
+        }
+      }
+    }
+
+    process(obj, root, const []);
+    return root;
+  }
+
+  List<Node> listAllNodes() {
+    final result = <Node>[];
+    void process(Node node) {
+      result.add(node);
+      if (node is ObjectNode) {
+        node.children.forEach(process);
+      }
+    }
+
+    process(this);
+    return result;
+  }
+
+  List<ObjectNode> listAllObjectNodes() {
+    final result = <ObjectNode>[];
+    void process(Node node) {
+      if (node is ObjectNode) {
+        result.add(node);
+        node.children.forEach(process);
+      }
+    }
+
+    process(this);
+    return result;
+  }
+}
+
+class ValueNode extends Node {
+  final String value;
+
+  ValueNode({
+    super.parent,
+    required super.keys,
+    required this.value,
+  });
+}
+
+class ObjectNode extends Node {
+  final children = <Node>[];
+
+  ObjectNode({
+    super.parent,
+    required super.keys,
+  });
+
+  String buildClassName([String prefix = "I18n"]) {
+    return "$prefix${keys.map((it) => it.toPascalCase()).join("\$")}";
+  }
+}
+
+extension _NodeExtension on Node {
+  ObjectNode get asObjectNode => this as ObjectNode;
+
+  ValueNode get asValueNode => this as ValueNode;
+
+  bool get isValue => this is ValueNode;
 }
